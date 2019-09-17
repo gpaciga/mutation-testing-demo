1446bb080cfcacbfbe8a6a389c8402c799f67121
Gregory Paciga <gpaciga@gmail.com>
2019-09-17 17:23:26 -0400

    updating to latest 0.x version before going to 1.x

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,16 +1,8 @@
 module.exports = function(config) {
   config.set({
-    files: [
-      "test/*.js",
-      {
-        pattern: "src/**/*.js",
-        mutated: true,
-        included: false
-      }
-    ],
     testRunner: "jest",
     mutator: "javascript",
     coverageAnalysis: "off",
-    reporter: ["html", "progress", "clear-text"]
+    reporters: ["html", "progress", "clear-text"]
   });
 };
